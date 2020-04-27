caecd63f1466a3127239377ea9f0f109e6ff63ca
Pedrag1234 <pedrag1234@gmail.com>
2020-04-27 18:06:45 +0100

    Time calculation now uses clock and threads can finish even after exec time

--- quarto/Qn.c
+++ quarto/Qn.c
@@ -1,7 +1,10 @@
 #include "Qn.h"
 
 //time is how long the server has been open, open_time is the max time the server CAN be open
-int currentTime, open_time, req_num = 0;
+clock_t start,currentTime;
+float open_time = 0;
+
+int  req_num = 0;
 pthread_mutex_t req_num_lock;
 
 void * processRequest(void *arg) {
@@ -15,7 +18,7 @@ void * processRequest(void *arg) {
     message->pid = getpid();
     message->tid = pthread_self();
 
-    if(currentTime < open_time) {
+    if((float)((currentTime - start)/CLOCKS_PER_SEC) < open_time) {
         
         //Increments request number 
         pthread_mutex_lock(&req_num_lock);
@@ -23,7 +26,6 @@ void * processRequest(void *arg) {
         pthread_mutex_unlock(&req_num_lock);
 
         message->pl = req_num;
-        currentTime += message->dur;
         usleep(message->dur * 1000); //use usleep bc time should be in the order of miliseconds and usleep is more precise
     }
 
@@ -44,6 +46,8 @@ void * processRequest(void *arg) {
 }
 
 int main(int argc, char const *argv[]) {
+    start = clock();
+
     BathroomParser * bp = createBathroomParser();
 
     if ((fillBathroomParser(argc, argv, bp) != 0) || strlen(bp->fifoname) == 0 || bp->secs_f == 0)
@@ -56,9 +60,8 @@ int main(int argc, char const *argv[]) {
     
     printBathroomParser(bp);
 
-    currentTime = 0;
     open_time = bp->nsecs;
-
+   
     char fifoname[64];
     strcpy(fifoname,bp->fifoname);
 
@@ -77,6 +80,7 @@ int main(int argc, char const *argv[]) {
 
     Message message;
     while(read(fd, & message, sizeof(message) )>0) {
+        currentTime = clock();
         pthread_t tid;
         //message must be void pointer because of the function create  
         pthread_create(& tid, NULL, processRequest, (void *) & message);
